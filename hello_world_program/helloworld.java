bb20b96c903e2c06fbaafc7466884ffa10ec0918
unknown <poojapatil010677@gmail.com>
2020-09-24 13:37:00 +0530

    Add hello world program in java

---
+++ hello_world_program/helloworld.java
@@ -0,0 +1,9 @@
+// Hello world program in java
+
+class HelloWorld {
+    public static void main(String[] args)
+    {
+        //print statement
+        System.out.println("Hello, World!");
+    }
+}
