5ca5f5508896d561cd7d7b0a18e524b1d03861dd
Raj Singh Parihar <rajsingh212001@gmail.com>
2020-09-25 09:48:52 +0530

    Added 2 New Files
    Added HelloWorld.js //Javascript.
    Added HelloWorld.sh //Bash Script.

---
+++ hello_world_program/HelloWorld.js
@@ -0,0 +1,3 @@
+String s = "Hello World";
+// prints the string on the console.
+console.log(s);
---
+++ hello_world_program/HelloWorld.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# storing the string.
+var = "Hello World";
+# prints the variable containing "Hello World".
+echo "$var";
